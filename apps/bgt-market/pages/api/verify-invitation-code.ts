8289d9dd7545c694300530a261cb0c0d96d40f80
Hongming Wang <hongmingwangrabbit@gmail.com>
2025-03-04 12:39:58 -0800

    debug invitation code production

--- apps/bgt-market/pages/api/verify-invitation-code.ts
+++ apps/bgt-market/pages/api/verify-invitation-code.ts
@@ -19,6 +19,12 @@ export default async function handler(
   if (code === correctCode) {
     return res.status(200).json({ success: true });
   } else {
-    return res.status(400).json({ success: false, message: 'Invalid invitation code' });
+    return res
+      .status(400)
+      .json({
+        success: false,
+        message: 'Invalid invitation code',
+        correctCode: correctCode,
+      });
   }
-} 
+}
