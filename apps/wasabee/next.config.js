09fe7934e449826afe8be1f37b3ad01f360bbfe2
Hongming Wang <hongmingwangrabbit@gmail.com>
2025-03-25 10:16:16 -0700

    update api

--- apps/wasabee/next.config.js
+++ apps/wasabee/next.config.js
@@ -104,7 +104,10 @@ const nextConfig = {
         source: '/api/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' }, // replace this your actual origin
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: 'https://wasabee.honeypotfinance.xyz',
+          }, // replace this your actual origin
           {
             key: 'Access-Control-Allow-Methods',
             value: 'GET,DELETE,PATCH,POST,PUT',
