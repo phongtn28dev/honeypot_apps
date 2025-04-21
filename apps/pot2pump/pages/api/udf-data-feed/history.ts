bfb3e66d369be17196d865ed2381c8e715a787e8
Hongming Wang <hongmingwangrabbit@gmail.com>
2025-04-20 20:21:34 -0700

    update api

--- apps/pot2pump/pages/api/udf-data-feed/history.ts
+++ apps/pot2pump/pages/api/udf-data-feed/history.ts
@@ -1,36 +1,28 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { trpc, trpcClient, trpcQueryClient } from '@/lib/trpc';
 import { resolutionType } from '@/services/priceFeed/priceFeedTypes';
-import { priceFeedRouter } from '@/server/router/priceFeed';
-import Trpc from '../trpc/[trpc]';
-import { appRouter, caller } from '@/server/_app';
-import BigNumber from 'bignumber.js';
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
-  //   Object.entries(req.query).forEach(([key, value]) => {
-  //     console.log(key, value);
-  //   });
+  const { priceFeedCaller } = await import('@/server/routers/priceFeedCaller');
 
   const ticker = req.query.symbol as string;
   const resolution = req.query.resolution as resolutionType;
   const from = req.query.from as string;
   const to = req.query.to as string;
 
-  const [
-    symbol,
-    chain,
-    address,
-    tokenNumber,
-    currencyCode,
-    totalSupply = '0',
-    quoteMetric = 'PRICE',
-  ] = ticker.split(':');
+  if (!ticker || !from || !to || !resolution) {
+    return res
+      .status(400)
+      .json({ s: 'error', errmsg: 'Missing required query parameters' });
+  }
+  const [symbol, chain, address, tokenNumber, currencyCode] = (
+    ticker || ''
+  ).split(':');
 
-  const data = await caller.priceFeed.getChartData({
+  const data = await priceFeedCaller.priceFeed.getChartData({
     chainId: chain,
     tokenAddress: address,
     from: parseInt(from),
@@ -41,25 +33,14 @@ export default async function handler(
   });
 
   if (data.status === 'success') {
-    // 当需要显示市值时，将价格乘以总供应量
-    if (quoteMetric === 'MCAP' && totalSupply && totalSupply !== '0') {
-      const supplyBN = new BigNumber(totalSupply);
-      const calculateMcap = (price: number) =>
-        new BigNumber(price).multipliedBy(supplyBN).toString();
-
-      res.status(200).json({
-        s: 'ok',
-        t: data.data.getBars.t,
-        o: data.data.getBars.o.map(calculateMcap),
-        h: data.data.getBars.h.map(calculateMcap),
-        l: data.data.getBars.l.map(calculateMcap),
-        c: data.data.getBars.c.map(calculateMcap),
-        v: data.data.getBars.v,
-      });
-    } else {
-      res.status(200).json({ s: 'ok', ...data.data.getBars });
-    }
+    res.status(200).json({
+      s: 'ok',
+      ...data.data.getBars,
+    });
   } else if (data.status === 'error') {
-    return res.status(500).json({ s: 'error', errmsg: data.message });
+    return res.status(500).json({
+      s: 'error',
+      errmsg: data.message,
+    });
   }
 }
