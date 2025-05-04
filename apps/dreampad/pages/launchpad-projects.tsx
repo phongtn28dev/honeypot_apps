9ee9e10ee80fdb71a16994dfd1afb6dde8905c9b
Hongming Wang <hongmingwangrabbit@gmail.com>
2025-05-04 14:35:13 -0700

    add a coming soon fto panel

--- apps/dreampad/pages/launchpad-projects.tsx
+++ apps/dreampad/pages/launchpad-projects.tsx
@@ -9,6 +9,7 @@ import { HoneyContainer } from '@/components/CardContianer';
 import { useLbpLaunchList } from '@honeypot/shared';
 import { Address } from 'viem';
 import { PaginationState } from '@honeypot/shared';
+import { Tab, Tabs } from '@nextui-org/react';
 
 const LbpProjectList = observer(() => {
   const { data, loading, error } = useLbpLaunchList();
@@ -33,77 +34,90 @@ const LbpProjectList = observer(() => {
 
   return (
     <HoneyContainer className="mx-auto max-w-[1280px]">
-      <DataContainer
-        hasData={data && data?.length > 0}
-        isLoading={loading}
-        className="w-full"
+      <Tabs
+        classNames={{
+          tabList: 'bg-[#202020] text-white',
+          tab: 'text-white',
+          panel: 'w-full',
+        }}
       >
-        <div className="w-full">
-          <motion.div
-            variants={defaultContainerVariants}
-            initial="hidden"
-            animate="visible"
-            className={'border border-[#5C5C5C] rounded-lg overflow-hidden'}
+        <Tab key="lbp" title="LBP">
+          <DataContainer
+            hasData={data && data?.length > 0}
+            isLoading={loading}
+            className="w-full"
           >
-            <table className="w-full">
-              <thead className="bg-[#323232] text-white border-b border-[#5C5C5C]">
-                <tr>
-                  <th>Project</th>
-                  <th>Status</th>
-                  <th>Raise Token</th>
-                  <th>Raise Amount</th>
-                  <th></th>
-                </tr>
-              </thead>
-              <tbody className="border-t border-[#F7931A0D] rounded-lg overflow-hidden">
-                {paginatedData?.map((pair, idx) => (
-                  <motion.tr
-                    variants={itemPopUpVariants}
-                    key={idx}
-                    className="bg-[#202020]  border-b-2 border-[#5C5C5C] "
-                  >
-                    <LaunchPadProjectCard
-                      status={pair.launchStatusDisplay}
-                      coverImg={pair.lbpBanner}
-                      isShowCoverImage={true}
-                      endDate={pair.endsAt.unix()}
-                      startDate={pair.startsAt.unix()}
-                      tokenName={pair.shareToken?.symbol ?? ''}
-                      projectAuthor={pair.owner}
-                      fundsRaised={pair.fundsRaised.toNumber()}
-                      assetTokenSymbol={pair.assetToken?.symbol ?? ''}
-                      shareTokenSymbol={pair.imageUrl}
-                      pairAddress={pair.address as Address}
-                      variant="list"
-                    />
-                  </motion.tr>
-                ))}
-              </tbody>
-            </table>
-            {data && data.length > 0 && (
-              <div className="flex justify-center items-center gap-2 py-4 bg-[#202020]">
-                <Button
-                  onPress={() => handlePageChange(currentPage - 1)}
-                  isDisabled={currentPage === 1}
-                  className="bg-[#323232] text-white"
-                >
-                  Previous
-                </Button>
-                <span className="text-white">
-                  Page {currentPage} of {paginationState.totalPage}
-                </span>
-                <Button
-                  onPress={() => handlePageChange(currentPage + 1)}
-                  isDisabled={currentPage === paginationState.totalPage}
-                  className="bg-[#323232] text-white"
-                >
-                  Next
-                </Button>
-              </div>
-            )}
-          </motion.div>
-        </div>
-      </DataContainer>
+            <div className="w-full">
+              <motion.div
+                variants={defaultContainerVariants}
+                initial="hidden"
+                animate="visible"
+                className={'border border-[#5C5C5C] rounded-lg overflow-hidden'}
+              >
+                <table className="w-full">
+                  <thead className="bg-[#323232] text-white border-b border-[#5C5C5C]">
+                    <tr>
+                      <th>Project</th>
+                      <th>Status</th>
+                      <th>Raise Token</th>
+                      <th>Raise Amount</th>
+                      <th></th>
+                    </tr>
+                  </thead>
+                  <tbody className="border-t border-[#F7931A0D] rounded-lg overflow-hidden">
+                    {paginatedData?.map((pair, idx) => (
+                      <motion.tr
+                        variants={itemPopUpVariants}
+                        key={idx}
+                        className="bg-[#202020]  border-b-2 border-[#5C5C5C] "
+                      >
+                        <LaunchPadProjectCard
+                          status={pair.launchStatusDisplay}
+                          coverImg={pair.lbpBanner}
+                          isShowCoverImage={true}
+                          endDate={pair.endsAt.unix()}
+                          startDate={pair.startsAt.unix()}
+                          tokenName={pair.shareToken?.symbol ?? ''}
+                          projectAuthor={pair.owner}
+                          fundsRaised={pair.fundsRaised.toNumber()}
+                          assetTokenSymbol={pair.assetToken?.symbol ?? ''}
+                          shareTokenSymbol={pair.imageUrl}
+                          pairAddress={pair.address as Address}
+                          variant="list"
+                        />
+                      </motion.tr>
+                    ))}
+                  </tbody>
+                </table>
+                {data && data.length > 0 && (
+                  <div className="flex justify-center items-center gap-2 py-4 bg-[#202020]">
+                    <Button
+                      onPress={() => handlePageChange(currentPage - 1)}
+                      isDisabled={currentPage === 1}
+                      className="bg-[#323232] text-white"
+                    >
+                      Previous
+                    </Button>
+                    <span className="text-white">
+                      Page {currentPage} of {paginationState.totalPage}
+                    </span>
+                    <Button
+                      onPress={() => handlePageChange(currentPage + 1)}
+                      isDisabled={currentPage === paginationState.totalPage}
+                      className="bg-[#323232] text-white"
+                    >
+                      Next
+                    </Button>
+                  </div>
+                )}
+              </motion.div>
+            </div>
+          </DataContainer>
+        </Tab>
+        <Tab key="FTO" title="FTO">
+          <div className="text-center text-2xl">Coming soon</div>
+        </Tab>
+      </Tabs>
     </HoneyContainer>
   );
 });
