51cb1642b03ee8ce365973e0c39401fc083226d4
Hongming Wang <hongmingwangrabbit@gmail.com>
2025-05-10 09:45:46 -0700

    init leaderboard

---
+++ apps/leaderboard/components/layout/index.tsx
@@ -0,0 +1,26 @@
+import { Header } from './header/v3';
+import { cn } from '@nextui-org/react';
+import ConfettiComponent from '@honeypot/shared/components/atoms/Confetti/Confetti';
+import { Footer } from './footer';
+import _ from 'lodash';
+
+export const Layout = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div
+      className={cn(
+        "flex flex-col min-h-screen w-full overflow-x-hidden overflow-y-auto bg-[url('/images/icons/bg-honey.png')]",
+        className
+      )}
+    >
+      <ConfettiComponent />
+      <Header />
+      <Footer />
+    </div>
+  );
+};
