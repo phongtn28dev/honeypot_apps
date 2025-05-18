e6d7a844dae62abb2a97ed8b39d00e2c0bd4396f
Hongming Wang <hongmingwangrabbit@gmail.com>
2025-05-18 11:09:43 -0700

    update package json

--- apps/wasabee/sentry.client.config.ts
+++
@@ -1,9 +0,0 @@
-// This file configures the initialization of Sentry on the client.
-// The config you add here will be used whenever a users loads a page in their browser.
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
-// NOTE: This file is being replaced by instrumentation-client.ts
-// We're keeping this file empty to avoid duplicate Sentry initialization
-// but not deleting it to maintain compatibility with existing imports.
-
-// Sentry initialization has been moved to instrumentation-client.ts
