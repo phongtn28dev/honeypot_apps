fed9fd30ce15682bb0f7f8792c8a39a33265ee03
sandro8ponali <phongtn@twendeesoft.com>
2025-05-30 09:22:43 +0700

    feat: update CountdownTimerProps and refactor AllInOneVault ABI structure

---
+++ apps/leaderboard/config/algebra/addresses.ts
@@ -0,0 +1,14 @@
+import { zeroAddress } from "viem";
+import { Address } from "viem";
+
+export const ALL_IN_ONE_VAULT: Address =
+    "0x5bA6c951F8AbCE6d507a922682690988E83ffbE0";
+
+export const ALL_IN_ONE_VAULT_PROXY: Address =
+    "0x9c52cD80455a9ee50610aC90e846e46E04014f6d";
+
+export const STAKING_TOKEN: Address = 
+    "0xa2b0519F1D7F61eaB95D486301ED924b58E97022";
+
+export const REWARD_VAULT: Address =
+    "0x234fc7Fde4C622E0bb973D21970c8F10cAa7B87B"
---
+++ apps/leaderboard/wagmi.config.ts
@@ -0,0 +1,26 @@
+import { ContractConfig, defineConfig } from '@wagmi/cli';
+import { actions, react } from '@wagmi/cli/plugins';
+import { ALL_IN_ONE_VAULT } from './config/algebra/addresses';
+import { AllInOneVaultABI } from './lib/abis/AllInOneVault';
+
+const contracts: ContractConfig[] = [
+  {
+    address: ALL_IN_ONE_VAULT,
+    abi: AllInOneVaultABI,
+    name: 'AllInOneVault',
+  },
+];
+
+export default defineConfig({
+  out: "./wagmi-generated.ts",
+  contracts,
+  plugins: [
+    actions({
+      //watchContractEvent: false,
+    }),
+    react({
+      // useContractEvent: false,
+      //useContractItemEvent: false,
+    }),
+  ],
+});
