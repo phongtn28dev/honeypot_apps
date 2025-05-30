d42f7715e37aba472b510b861377cc15105e471f
sandro8ponali <phongtn@twendeesoft.com>
2025-05-30 14:04:06 +0700

    feat: implement receipt management and update table structure for AllInOneVault

--- apps/leaderboard/next.config.js
+++ apps/leaderboard/next.config.js
@@ -12,6 +12,9 @@ const nextConfig = {
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
+  images: {
+    domains: ['cdn.prod.website-files.com'],
+  },
 };
 
 const plugins = [
